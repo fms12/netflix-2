392afb554fd4168527574608085266d48f555e63
fms12 <mayank2001.iimt@gmail.com>
2023-08-26 15:13:47 +0530

    videoplayer added

--- src/component/VideoPlayer/VideoPlayer.js
+++ src/component/VideoPlayer/VideoPlayer.js
@@ -2,8 +2,15 @@ import React from 'react'
 
 function VideoPlayer() {
   return (
-    <div>VideoPlayer</div>
-  )
+    <div>
+      <video
+        src="https://d2a4ei7putoo8k.cloudfront.net/video.mp4"
+        autoPlay={false}
+        loop
+        controls
+      />
+    </div>
+  );
 }
 
 export default VideoPlayer
